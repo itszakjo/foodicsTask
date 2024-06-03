a3d30d1b31fe5fe7542243961777e18d1b6758e4
immobby <itszakjo@gmail.com>
2024-06-03 17:37:59 +0300

    added login logic with throttling to limit login attempts per min

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-       //
+        if (!Auth::attempt($request->validated())) {
+            throw ValidationException::withMessages([
+                'message' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        // in a more advanced scenario, we can add ability/scope ['order-store'] to limit the token actions
+        $token = $request->user()->createToken('token-name')->plainTextToken;
+
+        return $this->respondWithToken($token);
+    }
+
+    private function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->middleware('throttle:10,1');
