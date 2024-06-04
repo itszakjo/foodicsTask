8267ff3ced14a56a5de6535404d3cb98234a23c5
immobby <itszakjo@gmail.com>
2024-06-04 23:53:53 +0300

    added Notification class for sending alert to the merchant

---
+++ app/Notifications/IngredientStockNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Ingredient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class IngredientStockNotification extends Notification
+{
+    use Queueable;
+
+    protected $ingredient;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Ingredient $ingredient
+     */
+    public function __construct(Ingredient $ingredient)
+    {
+        $this->ingredient = $ingredient;
+        $this->afterCommit();
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->greeting('Hello!')
+            ->line("The stock of {$this->ingredient->name} is below 50%.")
+            ->action('Restock Now', url('/inventory'))
+            ->line('Thank you for using our application!');
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'ingredient_id' => $this->ingredient->id,
+            'notification_type' => 'IngredientStock',
+        ];
+    }
+}
