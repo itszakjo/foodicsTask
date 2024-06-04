d0b04c8c4f55eedd2b056a8329636db977481815
immobby <itszakjo@gmail.com>
2024-06-05 01:07:47 +0300

    added api order routes with api version

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => '/v1'], function () {
 
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->middleware('throttle:10,1');
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->middleware('throttle:5,1');
+
+    Route::group(['prefix' => '/order', 'middleware'=>['auth:sanctum' , 'throttle:3600,3' ]], function () {
+        Route::post('/create', [App\Http\Controllers\OrderController::class, 'create']);
+    });
+});
+
+
