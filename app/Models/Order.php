adc023ce242b802bb3eb2b0bf516700f68966f6c
immobby <itszakjo@gmail.com>
2024-06-04 23:50:53 +0300

    added relations for order and orderItem models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id'];
+
+    public function items() : BelongsTo
+    {
+        return $this->belongsTo(Order::class , 'order_id');
+    }
 
 }
