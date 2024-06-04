cf2d3e3ee428de21f5250f202c11cc56b4ccca24
immobby <itszakjo@gmail.com>
2024-06-05 02:51:26 +0300

    add shouldQueue to notification

--- app/Notifications/IngredientStockNotification.php
+++ app/Notifications/IngredientStockNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class IngredientStockNotification extends Notification
+class IngredientStockNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
