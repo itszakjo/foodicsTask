300d9b076048c73ab8fabe2fccd22bddc152b10d
immobby <itszakjo@gmail.com>
2024-06-04 23:46:29 +0300

    modified order and product repositories to use corresponding interfaces

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -2,15 +2,29 @@
 
 namespace App\Repositories;
 
+use App\Contracts\Repositories\OrderRepositoryInterface;
 use App\Models\Order;
 
 class OrderRepository implements OrderRepositoryInterface
 {
-    public function find(int $id): Order
+
+    /**
+     * Find a model by its primary key.
+     *
+     * @param mixed $id
+     *
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection
+     */
+    public function find($id): Order
     {
         return Order::find($id);
     }
 
+    /**
+     * @param array $data
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function create(array $data): Order
     {
         return Order::create($data);
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,12 +2,30 @@
 
 namespace App\Repositories;
 
+use App\Contracts\Repositories\ProductRepositoryInterface;
 use App\Models\Product;
 
 class ProductRepository implements ProductRepositoryInterface
 {
-    public function find(int $id): Product
+    /**
+     * Find a model by its primary key.
+     *
+     * @param mixed $id
+     *
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection
+     */
+    public function find($id): Product
     {
         return Product::find($id);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function create(array $data): Product
+    {
+        return Product::create($data);
     }
 }
